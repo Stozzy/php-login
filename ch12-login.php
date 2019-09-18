fb706d8b8f425f7bb8c08c9fa0023ac71bb3bd62
Stozzy <sagoldyn@gmail.com>
2019-09-17 23:28:58 -0400

    init commit

---
+++ ch12-login.php
@@ -0,0 +1,24 @@
+<?php
+//start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+    <head>
+        <title>ch12 - Form Processing</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="ch12-login.php">Home Page</a></li>
+            <li><a href="ch12-protected-page.php">Protected Page</a></li>
+            <li><a href="#">Contact Page</a></li>
+            <li><a href="ch12-logout.php">Logout Page</a></li>
+        </ul>
+    </nav>
+
+</body>
+</html>
