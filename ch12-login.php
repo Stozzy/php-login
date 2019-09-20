3cfb1b36c35fee00e0db199adc57f5f4365fc055
Stozzy <sagoldyn@gmail.com>
2019-09-19 23:03:49 -0400

    added heredoc - website is broken now - need to determine what is going on.

--- ch12-login.php
+++ ch12-login.php
@@ -3,14 +3,13 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
+<html>
     <head>
         <title>ch12 - Form Processing</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
 
 <body>
-
     <nav>
         <ul>
             <li><a href="ch12-login.php">Home Page</a></li>
@@ -20,5 +19,28 @@ session_start();
         </ul>
     </nav>
 
+    <?php
+
+        if(isset($_SESSION['isLoggedIn'])) {
+
+            //the user logged in - don't show form and confuse the poor guy!
+            echo "<p>You are already logged in buddy!</p>";
+
+        } else {
+
+            //using heredoc, to echo out the form
+            $theForm = <<<FORM
+
+                <p>Welcome to WheatBook!</p>
+
+               <form method="post" action='ch12-login-response.php'>
+                   <input type='text' name='userName' id='username'>
+                   <input type='password' name='password'>
+                   <input type='submit'>
+               </form>
+            FORM;
+        }
+        echo $theForm;
+    ?>
 </body>
 </html>
